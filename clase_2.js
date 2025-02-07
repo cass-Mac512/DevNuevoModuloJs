6df34aafae33e94758444ba78af4140d0a5eed38
María Esther Macías Adame <m.macias@itnnovation.com>
2025-02-06 20:40:43 -0600

    first commit

---
+++ clase_2.js
@@ -0,0 +1,17 @@
+let notaEstudiante = 45;
+
+
+function evaluarNota(nota) {
+    if (nota >= 90) {
+        return "¡Excelente! Has aprobado con una calificación sobresaliente.";
+    } else if (nota >= 75) {
+        return "¡Bien! Has aprobado con una buena calificación.";
+    } else if (nota >= 60) {
+        return "Suficiente. Has aprobado, pero hay margen de mejora.";
+    } else {
+        return "Lo siento, no has aprobado. Necesitas esforzarte más la próxima vez.";
+    }
+}
+
+
+console.log(evaluarNota(notaEstudiante));
